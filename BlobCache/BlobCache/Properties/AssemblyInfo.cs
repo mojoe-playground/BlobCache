174dc770e76588ab81d4df92e7185cf0b2b3b3cc
mojoe-playground <mojoe@freemail.hu>
2017-07-20 13:34:43 +0200

    Added description and company to assembly info

--- BlobCache/BlobCache/Properties/AssemblyInfo.cs
+++ BlobCache/BlobCache/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BlobCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BlobCache")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mojoe")]
 [assembly: AssemblyProduct("BlobCache")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © mojoe 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
